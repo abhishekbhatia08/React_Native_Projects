cb1f70fb35228f6ae533c12ed80b0f7e0c1d5678
Abhishek Bhatia <abhibhatia2000@gmail.com>
2024-12-04 02:21:40 +0530

    Updated FirstProject: Modified app.tsx

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={Styles.container}>
+      <SafeAreaView>
+        <Text style={isDarkMode ? Styles.whiteText : Styles.blackText}>Helloo world!</Text>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: "#FFFFFF"
+  },
+  blackText: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: "#000000"
+  },
+});
+
+export default App;
