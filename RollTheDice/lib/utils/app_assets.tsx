fb92ef49a7876f8399a7a96bd14bbbebd32be858
Abhishek Bhatia <abhibhatia2000@gmail.com>
2024-12-26 20:02:41 +0530

    FEAT: Roll the Dice App

---
+++ RollTheDice/lib/utils/app_assets.tsx
@@ -0,0 +1,14 @@
+// src/assets/AppAssets.ts
+
+const AppAssets = {
+    diceImages: {
+        one: require('.../assets/One.png'),
+        two: require('.../assets/Two.png'),
+        three: require('.../assets/Three.png'),
+        four: require('.../assets/Four.png'),
+        five: require('.../assets/Five.png'),
+        six: require('.../assets/Six.png'),
+    }
+};
+
+export default AppAssets;
