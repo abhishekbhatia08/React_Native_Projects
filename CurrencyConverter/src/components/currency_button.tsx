881d8ebf5376ae9a05b57ad94b7ff7fefe8740ee
Abhishek Bhatia <abhibhatia2000@gmail.com>
2024-12-28 23:29:24 +0530

    ADD: Currency Converter Logics and UI

---
+++ CurrencyConverter/src/components/currency_button.tsx
@@ -0,0 +1,50 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { PropsWithChildren } from 'react'
+import React from 'react'
+
+type CurrencyProps = PropsWithChildren<{
+    flag: string,
+    name: string
+}>
+
+const CurrencyButton = (props: CurrencyProps): JSX.Element => {
+    return (
+        <View style={styles.button}>
+            <Text style={styles.flag}>{props.flag}</Text>
+            <Text style={styles.buttonText}>{props.name}</Text>
+        </View>
+    );
+}
+
+export default CurrencyButton;
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        borderRadius: 12,
+        padding: 10,
+        width: 120,
+        borderWidth: 4,
+        borderColor: '#333',
+        alignContent: 'center',
+        justifyContent: 'center',
+        elevation: 2,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowColor: '#333',
+        shadowOpacity: 0.1,
+        shadowRadius: 1,
+        height: 120
+    },
+    flag: {
+        fontSize: 30,
+        margin: 12
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#ffffff',
+        textAlign: 'center',
+    }
+})
